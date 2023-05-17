99bdd1d48fd5e8d6d01e8e765273d7d4bd3b40df
Poluvko-Tatyana <poluvko.tatyana@gmail.com>
2023-05-17 15:18:43 +0300

    1 завдання не закінчене

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,17 +1,43 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
 
 const gallery = document.querySelector(".gallery");
 
 const galleryItem = galleryItems.map (({preview, original, description}) => {
-    const galleryList =`<li><img src="${preview}" alt="${description} "></li>`;
+    const galleryList =`
+    <li>
+    <a class="gallery__link" href="${original}">
+    <img class = gallery__image src="${preview}" alt="${description} data-source="${original}">
+    </a>
+    </li>`;
     return galleryList;
 })
 
 gallery.insertAdjacentHTML('beforeend', galleryItem.join(''));
 
+gallery.addEventListener("click", onClickImg);
+
+function onClickImg(evt){
+    evt.preventDefault();
+    if(!evt.target.classList.contains("gallery__image")){
+        return;
+    }
 
 
-console.log(gallery)
+    // const source = evt.target.dataset.source
+    // const gallerySource = galleryItems.find(({original}) => source === original)
+    // console.log(gallerySource)
+
+   
+    const instance = basicLightbox.create(`
+    <img src="${evt.target.dataset.source}" width="800" height="600">
+`)
+
+instance.show()
+}
+
+
+
+
+
